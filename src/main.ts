eef2a435f781dcabaff7389638515e5449e37f67
Amer Kadic <akadic5@etf.unsa.ba>
2021-01-13 18:21:09 +0100

    switch

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { Transport } from '@nestjs/microservices';
+
+const logger = new Logger('Chat Socket');
+const microserviceOptions = {
+  transport: Transport.KAFKA,
+  options: {
+    client: {
+      brokers: ['localhost:9094'],
+    },
+    consumer: {
+      groupId: 'chat-socket-consumer',
+    },
+  },
+};
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    microserviceOptions,
+  );
+  await app.listen(() => {
+    logger.log('Microservice is listening...');
+  });
 }
 bootstrap();
